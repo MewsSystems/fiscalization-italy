fd5d0c8d0325af31e90c7315055cb7de9cf1145d
KaliCZ <kalandrapavel@gmail.com>
2021-02-19 18:07:45 +0100

    Allowed excluding taaxes based on article 15

--- src/Mews.Fiscalization.Italy/Constants/NormativeReference.cs
+++ src/Mews.Fiscalization.Italy/Constants/NormativeReference.cs
@@ -5,12 +5,15 @@ namespace Mews.Fiscalization.Italy.Constants
 {
     public static class NormativeReference
     {
+        public const string ExcludingArticle15 = "escluse ex. art. 15";
         public const string NotSubject = "non soggette";
 
         public static string GetByInvoiceLineKind(TaxKind taxKind)
         {
             switch (taxKind)
             {
+                case TaxKind.ExcludingArticle15:
+                    return ExcludingArticle15;
                 case TaxKind.NotSubject:
                     return NotSubject;
             }
